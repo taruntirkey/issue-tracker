9d405a4a261bb59a40ac98e04513d9b7b4c3f3e9
Tarun Tirkey <taruntirkey91@live.com>
2023-12-02 12:26:48 +0530

    Secure the application

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -2,10 +2,15 @@ import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 import { issueSchema } from "../../validationSchemas";
+import authOptions from "@/app/auth/authOptions";
+import { getServerSession } from "next-auth";
 
 type CreateIssueSchema = z.infer<typeof issueSchema>;
 
 export async function POST(request: NextRequest) {
+  const session = await getServerSession(authOptions);
+  if (!session) return NextResponse.json({}, { status: 401 });
+
   const body: CreateIssueSchema = await request.json();
   const validation = issueSchema.safeParse(body);
   if (!validation.success)
